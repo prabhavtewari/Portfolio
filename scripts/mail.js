3c3840065be05d955e682416ac73737525bdb619
Prabhav <prabhavtewari@gmail.com>
2021-09-27 14:52:15 +0530

    animations fix

--- scripts/mail.js
+++ scripts/mail.js
@@ -1,12 +1,11 @@
 // 
 
-var form=document.getElementById('cform');
-form.addEventListener('submit',function submit1(e)
-{
-  e.preventDefault()
-    var name=document.getElementById("name").value;
-    var email=document.getElementById("email").value;
-    var message=document.getElementById("message").value;
+var form = document.getElementById('cform');
+form.addEventListener('submit', function submit1(e) {
+    e.preventDefault()
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var message = document.getElementById("message").value;
     // console.log(name)
     // console.log(email)
     // console.log(message)
@@ -14,21 +13,19 @@ form.addEventListener('submit',function submit1(e)
     document.getElementById("email").value = " ";
     // document.getElementById("email") = "";
     document.getElementById("message").value = " ";
-    window.scrollTo(500, 0);
-    sendEmail(name,email,message);
+    window.scrollTo(0, 0);
+    sendEmail(name, email, message);
 });
 
-function sendEmail(name,email,message)
-{
+function sendEmail(name, email, message) {
     Email.send({
-        Host:"smtp.gmail.com",
+        Host: "smtp.gmail.com",
         Username: 'prabhavtewari@gmail.com',
-        Password:"nkmlyapdhsyyslom",
+        Password: "nkmlyapdhsyyslom",
         To: '1905331@kiit.ac.in',
-        From:'prabhavtewari@gmail.com',
-        Subject:`-Portfolio Message form ${name}`,
-        Body:`Name: ${name}<br>Email: ${email}<br>Message: ${message}`
+        From: 'prabhavtewari@gmail.com',
+        Subject: `-Portfolio Message form ${name}`,
+        Body: `Name: ${name}<br>Email: ${email}<br>Message: ${message}`
 
-    }).then((message)=>alert("Message Sent"))
+    }).then((message) => alert("Message Sent"))
 }
-
--- scripts/nav.js
+++ scripts/nav.js
@@ -4,23 +4,22 @@ const navLi = document.querySelectorAll('.navbar-nav li');
 document.querySelector(".home").classList.add("active")
 
 window.addEventListener('scroll', () => {
-  document.querySelector(".home").classList.remove("active")
-  let current = '';
-  sections.forEach(section => {
+    document.querySelector(".home").classList.remove("active")
+    let current = '';
+    sections.forEach(section => {
 
-    const sectionTop = section.offsetTop;
-    const sectionHeight = section.offsetTop;
-    if (scrollY + 100 > sectionTop) {
-      current = section.getAttribute("id");
+        const sectionTop = section.offsetTop;
+        // const sectionHeight = section.offsetTop;
+        if (scrollY + 100 > sectionTop) {
+            current = section.getAttribute("id");
 
-    }
-  });
-  navLi.forEach((li) => {
-    li.classList.remove("active");
-    if (li.classList.contains(current)) {
-      li.classList.add("active");
-    }
-  });
+        }
+    });
+    navLi.forEach((li) => {
+        li.classList.remove("active");
+        if (li.classList.contains(current)) {
+            li.classList.add("active");
+        }
+    });
 
 });
-
